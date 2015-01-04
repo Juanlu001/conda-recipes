35ead861224c13556dbfedbabf889377beb30c03
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2015-01-04 16:34:21 +0100

    Updated SuiteSparse to correct version Now dolfin can solve the linear system.

--- dolfin/build.sh
+++ dolfin/build.sh
@@ -2,5 +2,8 @@
 
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX
+cmake .. \
+  -DCMAKE_INSTALL_PREFIX=$PREFIX \
+  -DCMAKE_INCLUDE_PATH=$INCLUDE_PATH \
+  -DCMAKE_LIBRARY_PATH=$LIBRARY_PATH
 make install
