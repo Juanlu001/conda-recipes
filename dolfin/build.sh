b5c4510d6209d4d032882d614d516f792665dbe4
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2015-01-06 14:05:48 +0100

    Replaced suitesparse with petsc, added several requirements By suggestion of Garth N. Wells in the fenics-support mailing list, now AMD, UMFPACK and CHOLMOD are managed by PETSc, which simplifies the build process. This commit added corresponding conda-recipes for PETSc, PETSc4Py and SLEPc.

--- dolfin/build.sh
+++ dolfin/build.sh
@@ -2,6 +2,11 @@
 
 mkdir build
 cd build
+
+export AMD_DIR=$SP_DIR/petsc
+export UMFPACK_DIR=$SP_DIR/petsc
+export CHOLMOD_DIR=$SP_DIR/petsc
+
 cmake .. \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
   -DCMAKE_INCLUDE_PATH=$INCLUDE_PATH \
