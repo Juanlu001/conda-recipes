718915f7277bcb2e006ce6e59c68e3008018252e
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2015-01-11 13:33:16 +0100

    Cleaned unused patches, specified BLAS directory

--- dolfin/build.sh
+++ dolfin/build.sh
@@ -3,9 +3,9 @@
 mkdir build
 cd build
 
+export BLAS_DIR=$LIBRARY_PATH
 export AMD_DIR=$SP_DIR/petsc
 export UMFPACK_DIR=$SP_DIR/petsc
-export CHOLMOD_DIR=$SP_DIR/petsc
 
 cmake .. \
   -DCMAKE_INSTALL_PREFIX=$PREFIX \
